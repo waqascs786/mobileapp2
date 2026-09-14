470645056ccb9df1b98c90ac4d4554ce47fe3057
waqascsme <waqascsme@gmail.com>
2026-09-14 23:24:20 +0500

    Fix package name sed pattern (_app suffix), add INTERNET permission, harden main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -25,9 +25,7 @@ void main() async {
     DeviceOrientation.portraitDown,
   ]);
 
-  try {
-    await AppConfig.load();
-  } catch (_) {}
+  AppConfig.load();
 
   final storageService = StorageService();
   try {
